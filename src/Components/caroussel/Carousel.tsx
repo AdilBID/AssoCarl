eef77e6ac487f1ffcebe8a79e884df67acec248f
SarraVDE <friaa.sarra@yahoo.fr>
2023-12-07 14:52:58 +0100

    Carrousel dynamique

---
+++ src/Components/caroussel/Carousel.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import '../caroussel/_caroussel.scss';
+
+interface ImageData {
+  url: string;
+  text: string;
+  buttonLabel: string;
+  buttonLink: string;
+}
+
+interface CarouselProps {
+  images: ImageData[];
+}
+
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <div className='carouss'>
+      <div className='mgs'>
+        <img className={`mesImages ${currentIndex === 0 ? 'active' : ''}`} src={images[currentIndex].url} alt={`slide-${currentIndex}`} />
+
+        <div className='overlay-content'>
+          {/* Utilisez une classe dynamique pour forcer la réactualisation de l'animation */}
+          <p className={`font_0 wixui-rich-text__text sliding-animation force-reload-${currentIndex}`}>
+            {images[currentIndex].text}
+          </p>
+          <a
+            href={images[currentIndex].buttonLink}
+            target='_blank'
+            rel='noreferrer noopener'
+            className='StylableButton2545352419__root style-lfxwzlwa__root wixui-button StylableButton2545352419__link'
+            aria-label='Souscrire'
+          >
+            <div className='StylableButton2545352419__container'>
+              <span className='StylableButton2545352419__label wixui-button__label'>{images[currentIndex].buttonLabel}</span>
+              <span className='StylableButton2545352419__icon wixui-button__icon' aria-hidden='true'></span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <button className="carousel-button prev" onClick={handlePrev}>&lt;</button>
+      <button className="carousel-button next" onClick={handleNext}>&gt;</button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,18 @@
 // pages/Home.tsx
 import React from "react";
 import CardList from "../Components/cardList/CardList";
-import Carousel from "../Components/Carousel";
+import Carousel from "../Components/caroussel/Carousel";
 import Dessin from "../assets/img/Draw.jpg";
-import Enfants from "../assets/img/Draw.jpg";
-import Rue from "../assets/img/Enfants.jpg";
+import Enfants from "../assets/img/Enfants.jpg";
+import Rue from "../assets/img/Rue.jpg";
 import Presentation from "../Components/presentation/Presentation";
 
-const images = [
-  Dessin,
-  Enfants,
-  Rue,
+const images: any = [
+  { url: Dessin, text: "Texte pour Dessin", buttonLabel: "Bouton pour Dessin", buttonLink: "https://lien-pour-draw" },
+  { url: Enfants, text: "Texte pour Enfants", buttonLabel: "Bouton pour Enfants", buttonLink: "https://lien-pour-enfants" },
+  { url: Rue, text: "Texte pour Rue", buttonLabel: "Bouton pour Rue", buttonLink: "https://lien-pour-rue" },
 ];
+
 
 const Home: React.FC = () => {
   return (
